1f6d3c70da6093856f606000582b32695f43d6ab
Jungmoo Yoon <alexjm@naver.com>
2023-01-16 16:14:56 +0900

    5.5

--- Day03_missio.py
+++ Day03_missio.py
@@ -1,5 +1,15 @@
-# #5.4
-#
+# 5.5
+salutation = "hello"
+name= "alex"
+product= "knife"
+verbed="scratch"
+room ="402"
+animals= "cat"
+amount ="100$"
+percent= "75"
+spokesman= "David"
+job_title= "manager"
+
 letter = """ Dear {0} {1}
           Thank you for your letter. We are sorry that our  {2} {3} in your {4}. Please note that it should never be used in a {4},especially near any {5}
           Send us your receipt and {6} for shipping and handling. We will send you another {2} that, in our tests, is {7}% less likely to have {3}.
@@ -8,3 +18,4 @@ letter = """ Dear {0} {1}
          {8}
          {9}""".format(salutation,name,product,verbed,room,animals,amount,percent,spokesman,job_title)
 
+print(letter)
