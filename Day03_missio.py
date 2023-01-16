2809f8fc6bc007bc3f38c930650cb4993c9c2754
Jungmoo Yoon <alexjm@naver.com>
2023-01-16 16:04:25 +0900

    5.4

---
+++ Day03_missio.py
@@ -0,0 +1,10 @@
+# #5.4
+#
+letter = """ Dear {0} {1}
+          Thank you for your letter. We are sorry that our  {2} {3} in your {4}. Please note that it should never be used in a {4},especially near any {5}
+          Send us your receipt and {6} for shipping and handling. We will send you another {2} that, in our tests, is {7}% less likely to have {3}.
+         Thank you for your support.
+         Sincerely,
+         {8}
+         {9}""".format(salutation,name,product,verbed,room,animals,amount,percent,spokesman,job_title)
+
