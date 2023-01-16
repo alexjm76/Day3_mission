d3ca2ab63a0c7a138f90b5506a7eaef06339aa6e
Jungmoo Yoon <alexjm@naver.com>
2023-01-16 16:26:23 +0900

    5.7

--- Day03_missio.py
+++ Day03_missio.py
@@ -1,10 +1,10 @@
-# 5.6
+# 5.7
 name1 = "Duck"
 name2 = "Gourd"
 name3 = "Spitz"
-example1= "%sy Mc%sface" % (name1 , name1)
-example2= "%sy Mc%sface" % (name2 , name2)
-example3= "%sy Mc%sface" % (name3 , name3)
+example1= "{0}y Mc{0}face".format(name1)
+example2= "{0}y Mc{0}face".format(name2)
+example3= "{0}y Mc{0}face".format(name3)
 print(example1)
 print(example2)
 print(example3)
